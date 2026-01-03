2d1b7fbb7f896be2c69d0e871fb33ebdd5c233a8
TechStarrr <millicentujunwa12@gmail.com>
2026-01-04 00:15:43 +0100

    implemented checkin function

--- src/EventItCheckIn.sol
+++ src/EventItCheckIn.sol
@@ -47,4 +47,21 @@ contract EventItCheckIn is Initializable, OwnableUpgradeable, UUPSUpgradeable {
         operators[eventId][operator] = allowed;
         emit OperatorSet(eventId, operator, allowed);
     }
+
+    function checkIn(uint256 eventId, uint256 tokenId, bool burnAfter)
+        external
+        onlyOperator(eventId)
+    {
+        require(!usedTickets[eventId][tokenId], "EventItCheckIn: used");
+        require(ticket.ownerOf(tokenId) != address(0), "EventItCheckIn: missing");
+        require(ticket.eventOf(tokenId) == eventId, "EventItCheckIn: wrong event");
+
+        usedTickets[eventId][tokenId] = true;
+
+        if (burnAfter) {
+            ticket.burn(tokenId);
+        }
+
+        emit CheckedIn(eventId, tokenId, msg.sender, burnAfter);
+    }
 }
